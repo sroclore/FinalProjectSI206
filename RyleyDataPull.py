eef38d2384ef85cb2b714c1153021aaec4ebd02c
Ryley Larson <rlarson.ump@gmail.com>
2022-04-21 14:16:20 -0400

    Created Database. Need to write code to add year by year into Database. Then need to create visualizations.

--- RyleyDataPull.py
+++ RyleyDataPull.py
@@ -1,15 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import sqlite3
 
 #Step 1a: Get attendance numbers for 2021-2012
-def getAttendanceMLB():
+def getAttendanceMLB(year):
     baseURL = 'https://www.espn.com/mlb/attendance/_/year/'
 
-    years = ['2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012']
-    for year in years:
+    try:
+        year = str(year)
         r = requests.get(f'{baseURL}{year}')
         soup = BeautifulSoup(r.text, 'html.parser')
         rows = soup.find_all('tr')
+    except:
+        print('Exception')
 
     info = []
     for row in rows[2:]:
@@ -21,11 +25,21 @@ def getAttendanceMLB():
 
     return info
 
-print(len(getAttendanceMLB()))
+#Step 2: Add attendance numbers and teams to SQL
+def createDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    
+    cur.execute('CREATE TABLE IF NOT EXISTS AttendanceMLB (team_id INTEGER PRIMARY KEY, teamName TEXT, avgAttendance INTEGER, totAttendance INTEGER')
+    conn.commit()
+    return cur, conn
 
-#Step 1b: Get teams from ESPN api
-
-#Step 2: Add attendance numbers and teams to SQL (team table and attendance table, shared team id)
+def addData(year, db_name):
+    cur, conn = createDatabase(db_name)
+    data = getAttendanceMLB(year)
+    
+    
 
 #Step 3: Create visualizations and such
 
