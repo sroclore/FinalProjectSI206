ab3bc4d1d0a3dee35109bcc8611a5f6e187e937b
Ryley Larson <rlarson.ump@gmail.com>
2022-04-24 20:41:06 -0400

    blank canvas

---
+++ createVisualizations.py
@@ -0,0 +1,11 @@
+import sqlite3
+import os
+import matplotlib.pyplot as plt
+
+
+def openDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
