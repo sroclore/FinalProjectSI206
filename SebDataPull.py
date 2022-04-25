ce07473be66d2dcf352f6f2e0049120333725798
sroclore <sroclore@umich.edu>
2022-04-24 21:17:43 -0400

    hey

--- SebDataPull.py
+++ SebDataPull.py
@@ -61,33 +61,43 @@ def getAttendancePrem(year):
 
 
 
-# def prem_data_grab(season):
-#     L = get_teams_by_season(season)
+def prem_data_grab(season):
+    # L = get_teams_by_season(season)
 
-#     url = "https://football-web-pages1.p.rapidapi.com/attendances.json"
+    url = "https://football-web-pages1.p.rapidapi.com/attendances.json"
 
-#     headers = {
-#         "X-RapidAPI-Host": "football-web-pages1.p.rapidapi.com",
-#         "X-RapidAPI-Key": "e34cfc2358mshb0d0a32b739f2e0p1fa103jsnf9f3985beab7"
-#     }
+    headers = {
+        "X-RapidAPI-Host": "football-web-pages1.p.rapidapi.com",
+        "X-RapidAPI-Key": "e34cfc2358mshb0d0a32b739f2e0p1fa103jsnf9f3985beab7"
+    }
 
-#     data_dict = {}
+    data_dict = {}
     
-#     for i in range(1, 21):
-#         querystring = {"comp":"1","team":{i},"sort":"average","type":"home"}
-#         response = requests.request("GET", url, headers=headers, params=querystring)
-#         json_data = json.loads(response.text)
-#         for x,y in json_data["attendances"].items():
-#             if x == 'team':
-#                 if y['name'] in L:
-#                     team = y['name']
-#                     data_dict[team] = []
-#             elif x == 'matches':
-#                 for match in y:
-#                     if match["attendance"] != 0:
-#                         data_dict[team].append(match["attendance"])
+    for i in range(1, 21):
+        querystring = {"comp":"1","team":{i},"sort":"average","type":"home"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        json_data = json.loads(response.text)
+        for x,y in json_data["attendances"].items():
+            if x == 'team':
+                # if y['name'] in L:
+                    team = y['name']
+                    data_dict[team] = []
+            elif x == 'matches':
+                for match in y:
+                    if match["attendance"] != 0:
+                        data_dict[team].append(match["attendance"])
     
-#     print(data_dict)
+    for x,y in data_dict.items():
+        sum = 0
+        tot = 0
+        for num in y:
+            sum += num
+            tot += 1
+        data_dict[x] = sum / tot
+    
+    print(data_dict)
+    print(len(data_dict))
+    return data_dict
 
 
 
@@ -100,7 +110,7 @@ def getAttendancePrem(year):
 
 
 def main():
-    # prem_data_grab("2021-2022")
+    prem_data_grab("2021-2022")
     # print(get_teams_by_season("2013-2014"))
     print(getAttendancePrem("2021-2022"))
     return 1
